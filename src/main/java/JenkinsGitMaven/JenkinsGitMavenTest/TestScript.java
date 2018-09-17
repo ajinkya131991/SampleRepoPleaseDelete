a6f16184be0de69afddb74551c604b65950688ea
ajinkya131991 <ajinkyabhobad@gmail.com>
2018-09-16 22:58:28 -0700

    This is my first commit

---
+++ src/main/java/JenkinsGitMaven/JenkinsGitMavenTest/TestScript.java
@@ -0,0 +1,44 @@
+package JenkinsGitMaven.JenkinsGitMavenTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestScript 
+{
+	WebDriver driver;
+	@BeforeTest
+	public void setUp()
+	{
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\admin\\eclipse-workspace\\JenkinsGitMavenTest\\drivers\\geckodriver-v0.20.0-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+	@Test
+    public void test()
+    {
+    	
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+
+        driver.get(baseUrl);
+
+        actualTitle = driver.getTitle();
+        
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+    }
+        
+        @AfterTest
+        public void tearDown()
+        {
+        driver.close();
+        }
+
+    }
+
